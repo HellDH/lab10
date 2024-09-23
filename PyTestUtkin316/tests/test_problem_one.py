f294e4c172b90fe5ecd80447a7ada75abccbbd5c
Student <Student@academy.int>
2024-09-23 15:59:15 +0300

    testing update

--- PyTestUtkin316/tests/test_problem_one.py
+++ PyTestUtkin316/tests/test_problem_one.py
@@ -1,23 +1,27 @@
 import os
 import sys
+import pytest
 
-parent_directory = os.path.abspath('..')
+parent_directory = os.path.abspath('...')
 
 sys.path.append(parent_directory)
 
+from typing import Dict, Tuple
 from PyTestUtkin316.utils import TriangleProblemOne
 
-def test_scalene():
+@pytest.mark.parametrize("data",
+                         [
+                             {"input": (1, 2, 3), "expected": 1},
+                             {"input": (4, 2, 2), "expected": 1},
+                             {"input": (4, 4, 4), "expected": 0.5}
+                         ])
+def test_scalene(data: Dict[Tuple[int|float,
+                                  int|float,
+                                  int|float], int]):
     o = TriangleProblemOne()
-    ans = o.defineTriangle(1, 2, 3)
-    assert ans == 1.0
 
-def test_isosceles():
-    o = TriangleProblemOne()
-    ans = o.defineTriangle(4, 2, 2)
-    assert ans == 1.0
+    s = data["input"]
 
-def test_equilateral():
-    o = TriangleProblemOne()
-    ans = o.defineTriangle(4, 4, 4)
-    assert ans == 0.5
+    ans = o.defineTriangle(s[0], s[1], s[2])
+
+    assert ans == data["expected"]
